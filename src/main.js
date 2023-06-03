5abfa4c710acfd89c046def360936a8f90f21ac0
糖果不太甜 <2293342913@qq.com>
2023-06-04 01:11:26 +0800

    scss文件的自动导入

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import './assets/main.css'
+
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// 框架自带的样式文件
+// import './assets/main.css'
+// 自己编写的样式文件
+import  "@/styles/common.scss";
 // import {  getCategoryAPI} from "@/apis/testApi";
 // getCategoryAPI().then((res)=>{ 
 // console.log(res);
