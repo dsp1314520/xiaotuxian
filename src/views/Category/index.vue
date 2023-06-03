b58a10427cadb01a593bb4df46d135e90309479f
糖果不太甜 <2293342913@qq.com>
2023-06-03 21:51:07 +0800

    路由的基本配置完成

---
+++ src/views/Category/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    我是Category页面
+  </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss" scoped></style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    我是home页面
+  </div>
+</template>
+<script setup>
+
+
+</script>
+<style lang="scss" scoped></style>
