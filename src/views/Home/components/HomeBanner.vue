3cb6dca01d1e4d6000672e17ebe3f91c19d1877e
糖果不太甜 <2293342913@qq.com>
2023-06-05 21:41:53 +0800

    Home整体结构拆分，以及分类完成

---
+++ src/views/Home/components/HomeBanner.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    我是banner
+  </div>
+</template>
+<script setup>
+
+
+</script>
+<style lang="scss" scoped></style>
---
+++ src/views/Home/components/HomeHot.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <div>
+    我是人气推荐
+  </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss" scoped></style>
