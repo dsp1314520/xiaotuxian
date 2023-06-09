c106a49990c211e3d38771c66c3623c7beccdf3d
糖果不太甜 <2293342913@qq.com>
2023-06-09 13:15:09 +0800

    路由缓存问题解决

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,7 +2,7 @@
 import { getTopCategoryAPI } from '@/apis/category'
 import { getBannerAPI } from '@/apis/home'
 import { ref, watch } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, onBeforeRouteUpdate } from "vue-router";
 import GoodsItem from "@/components/GoodsItem.vue";
 const categoryData = ref({})
 const route = useRoute()
@@ -21,12 +21,17 @@ const getBanner = async () => {
   bannerList.value = res.result
 }
 getBanner()
-watch(route, () => {
-  // console.log(132123);
-  getCategory(route.params.id)
-  getBanner()
-}, { deep: true })
-
+// 使用watch监听路由变化
+// watch(route, () => {
+//   // console.log(132123);
+//   getCategory(route.params.id)
+// }, { deep: true })
+// 使用vue-router自己的方法监听路由变化
+onBeforeRouteUpdate((to) => {
+  // console.log('路由变化了');
+  // console.log(to.params.id, 123123);
+  getCategory(to.params.id)
+})
 
 </script>
 
